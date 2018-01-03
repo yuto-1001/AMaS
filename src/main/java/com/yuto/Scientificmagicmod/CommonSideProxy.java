0ec215a8717b3703c32811771494fa7a7945b7e2
yuto <yuto1001channel@gmail.com>
2018-01-03 16:08:46 +0900

    ふぁいるの配置を整えた

---
+++ src/main/java/com/yuto/Scientificmagicmod/CommonSideProxy.java
@@ -0,0 +1,15 @@
+package com.yuto.Scientificmagicmod.client;
+
+import net.minecraft.world.World;
+
+public class CommonSideProxy {
+	public void registerRenderers()
+	{
+
+	}
+
+	public World getClientWorld() {
+
+		return null;
+	}
+}
