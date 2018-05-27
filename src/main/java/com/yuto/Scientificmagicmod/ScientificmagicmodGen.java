e0afeec82f8e3e7650af8dc462b83acafe1c556f
yuto <yuto1001channel@gmail.com>
2018-05-27 19:00:11 +0900

    BulbBread追加。 BWsword削除ー＞一つにまとめてShift右クリックで変えられるようにする。 彼岸花ファイルまとめてメタデータで管理。

--- src/main/java/com/yuto/Scientificmagicmod/ScientificmagicmodGen.java
+++ src/main/java/com/yuto/Scientificmagicmod/ScientificmagicmodGen.java
@@ -14,25 +14,13 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 public class ScientificmagicmodGen implements IWorldGenerator {
 	public WorldGenerator Redbeanore;
 	public WorldGenerator grass;
-	public WorldGenerator CA_r;
-	public WorldGenerator CA_w;
-	public WorldGenerator CA_b;
-	public WorldGenerator CA_bl;
 	public ScientificmagicmodGen(){
 		Redbeanore = new WorldGenMinable(ScientificmagicBlock.Redbeanore, 8);
 		grass = new WorldGenMinable(Blocks.grass, 1);
-		CA_r = new WorldGenMinable(ScientificmagicBlock.CA_r, 1,20, Blocks.grass);
-		CA_w = new WorldGenMinable(ScientificmagicBlock.CA_w, 1,20, Blocks.grass);
-		CA_b = new WorldGenMinable(ScientificmagicBlock.CA_b, 1,20, Blocks.grass);
-		CA_bl = new WorldGenMinable(ScientificmagicBlock.CA_bl, 1,20, Blocks.grass);
 	}
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,IChunkProvider chunkProvider) {
 		this.RedGen(Redbeanore, world, random, chunkX, chunkZ, 8, 0, 20);
-		this.CAGen(CA_r, world, random, chunkX, chunkZ, 10, 0, 200);
-		this.CAGen1(CA_w, world, random, chunkX, chunkZ, 10, 0, 200);
-		this.CAGen2(CA_b, world, random, chunkX, chunkZ, 10, 0, 200);
-		this.CAGen3(CA_bl, world, random, chunkX, chunkZ, 10, 0, 200);
 	}
 	private void RedGen(WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight){
 		int heightDiff = maxHeight - minHeight + 1;
@@ -41,46 +29,6 @@ public class ScientificmagicmodGen implements IWorldGenerator {
 			int y = ( minHeight + rand.nextInt(heightDiff) )+ 2;
 			int z = chunkZ * 16 + rand.nextInt(16);
 			Redbeanore.generate(world, rand, x, y, z);
-		}
-	}
-	private void CAGen(WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight){
-		int heightDiff = maxHeight - minHeight + 1;
-		for (int i = 0; i < chancesToSpawn; i ++){
-			int x = chunkX * 16 + rand.nextInt(16);
-			int y = ( minHeight + rand.nextInt(heightDiff) )+ 2;
-			int z = chunkZ * 16 + rand.nextInt(16);
-			grass.generate(world, rand, x, y, z);
-			CA_r.generate(world, rand, x, y + 1, z);
-		}
-	}
-	private void CAGen1(WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight){
-		int heightDiff = maxHeight - minHeight + 1;
-		for (int i = 0; i < chancesToSpawn; i ++){
-			int x = chunkX * 16 + rand.nextInt(16);
-			int y = ( minHeight + rand.nextInt(heightDiff) )+ 2;
-			int z = chunkZ * 16 + rand.nextInt(16);
-			grass.generate(world, rand, x, y, z);
-			CA_w.generate(world, rand, x, y + 1, z);
-		}
-	}
-	private void CAGen2(WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight){
-		int heightDiff = maxHeight - minHeight + 1;
-		for (int i = 0; i < chancesToSpawn; i ++){
-			int x = chunkX * 16 + rand.nextInt(16);
-			int y = ( minHeight + rand.nextInt(heightDiff) )+ 2;
-			int z = chunkZ * 16 + rand.nextInt(16);
-			grass.generate(world, rand, x, y, z);
-			CA_b.generate(world, rand, x, y + 1, z);
-		}
-	}
-	private void CAGen3(WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight){
-		int heightDiff = maxHeight - minHeight + 1;
-		for (int i = 0; i < chancesToSpawn; i ++){
-			int x = chunkX * 16 + rand.nextInt(16);
-			int y = ( minHeight + rand.nextInt(heightDiff) )+ 2;
-			int z = chunkZ * 16 + rand.nextInt(16);
-			grass.generate(world, rand, x, y, z);
-			CA_bl.generate(world, rand, x, y + 1, z);
 		}
 	}
 }
